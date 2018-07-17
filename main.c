779133f37b442c21b7409281f4271d8617858758
Saurabh Sharma <saurabhsvits@gmail.com>
2018-07-17 06:14:25 -0400

    Implementation  : Update name
    Update the name to be printed in lsh-help

--- main.c
+++ main.c
@@ -67,7 +67,7 @@ int lsh_cd(char **args)
 int lsh_help(char **args)
 {
   int i;
-  printf("Stephen Brennan's LSH\n");
+  printf("Saurabh's version of Stephen B's LSH\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
